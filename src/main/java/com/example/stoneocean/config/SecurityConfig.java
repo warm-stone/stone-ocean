4902a97228758665991a4a7fe69f445b14dda001
root <root@codeagent-n5105.home>
2026-06-17 15:50:16 +0000

    fix(security): revert login/token from permitAll, keep authenticated for httpBasic

--- src/main/java/com/example/stoneocean/config/SecurityConfig.java
+++ src/main/java/com/example/stoneocean/config/SecurityConfig.java
@@ -43,8 +43,6 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/oauth2Login/**",
                                 "/user/add",
-                                "/user/login",
-                                "/token/token",
                                 "/rankList/page",
                                 "/rankList/member/**",
                                 "/rankMember/member/**",
